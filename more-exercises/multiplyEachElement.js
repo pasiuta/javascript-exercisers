9e3965807bc341298af5971654d54e5342357ea9
dima <pasuta.dima@gmail.com>
2018-07-26 12:17:05 +0300

    fix bags

--- more-exercises/multiplyEachElement.js
+++ more-exercises/multiplyEachElement.js
@@ -1,10 +1,20 @@
+'use strict';
 
+function multiplyEachElement(number) {
+    let array = [];
+    while (number > 0) {
+        array.push(number * 2);
+        number--;
+    }
+    return array.join('-');
+}
 
-
+const a = multiplyEachElement(11);
+console.log(a);
 
 
 //Написать функцию, которая принимает один параметр(числовой)
 // и выводит числовую последовательность от введенного число до 0 через тире,
 // где каждый парный елемент уможен на два и через тире.
-   // Пример: func(10)
-//20-16-14-12-10-8-4-2-0
+// Пример: func(10)
+//20-18-16-14-12-10-8-6-4-2-0
